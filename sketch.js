a31240dfd651ce6b7938a650a3937b5246e3bd7c
“Jen <Jen.j.eyring@gmail.com>
2022-09-17 15:19:39 -0500

    adding parameters

---
+++ sketch.js
@@ -0,0 +1,24 @@
+// module aliases
+var Engine = Matter.Engine
+   // Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+var engine;
+var world;
+var box1;
+
+function setup() {
+    createCanvas(400, 400);
+    engine = Engine.create();
+    world = engine.world;
+    box1 = Bodies.rectangle(200, 100, 80, 80);
+    Engine.run(engine);
+    World.add(world, box1);
+}
+
+function draw() {
+    background(51);
+
+    rect(box1.position.x, box1.position.y, 80, 80)
+}
